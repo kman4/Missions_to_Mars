a305ebf0b8624c4476a40fa8e3a3edc99360332f
Kapil Pundhir <70111855+kman4@users.noreply.github.com>
2020-12-28 16:40:15 -0500

    commit

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 ### IMPORT DEPENDENCIES
+! pip install pymongo
+
 
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
-import pymongo
+
 
 # Create an instance of Flask
 app = Flask(__name__)
