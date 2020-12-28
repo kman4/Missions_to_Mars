af3fa307096cc39129ff997e1b1f22312bff095c
Kapil Pundhir <70111855+kman4@users.noreply.github.com>
2020-12-28 13:22:15 -0500

    commit

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 ### IMPORT DEPENDENCIES
 
-from flask import Flask, render_template, redirect,jsonify
+from flask import Flask, render_template, redirect
 from flask_pymongo import pymongo 
 import scape
 
 # INITIATE YOUR CONFIGURATION 
 app = Flask(__name__)
-app.config["MONGO_URI"] = 'mongodb://localhost:27017'
+app.config["MONGO_URI"] = 'mongodb://localhost:27017/mars_db'
 mongo = PyMongo(app)
 
 @app.route("/")
-def index()
+def index():
     mars_data = db.collection.find_one()
     return render_template('index.html', list=mars_data)
                            
 @appl.route("/scrape")
-def scrape()
+def scrape():
     scrape.scrape_all()
-    retunr redirect("/")                       
+    return redirect("/")                       
                            
+ 
--- srape.py
+++ srape.py
@@ -1,108 +1,113 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import requests\n",
-    "import json\n",
-    "from bs4 import beautifulsoup\n",
-    "import splinter\n"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "executable_path = {'executable_path' : 'chromedriver.exe'}\n",
-    "browser = splinter.Browser(chrome\", ** executable_path)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "url = 'https://mars.nasa.gov/news/'\n"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "html = browser.html\n",
-    "soup = bs(html,'html parser')\n",
-    "featured_img = soup.find(\"img\", class_='main_image')['src']"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "featured_img"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "pic_url = f\"https;?/www.jpl.nada.gov{featured_img}''"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "pic_url"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.3"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+# coding: utf-8
+
+#Imports & Dependencies
+from splinter import Browser
+from bs4 import BeautifulSoup
+
+#Site Navigation
+executable_path = {"executable_path": "/Users/sharonsu/Downloads/chromedriver"}
+browser = Browser("chrome", **executable_path, headless=False)
+
+
+# Defining scrape & dictionary
+def scrape():
+    final_data = {}
+    output = marsNews()
+    final_data["mars_news"] = output[0]
+    final_data["mars_paragraph"] = output[1]
+    final_data["mars_image"] = marsImage()
+    final_data["mars_weather"] = marsWeather()
+    final_data["mars_facts"] = marsFacts()
+    final_data["mars_hemisphere"] = marsHem()
+
+    return final_data
+
+# # NASA Mars News
+
+def marsNews():
+    news_url = "https://mars.nasa.gov/news/"
+    browser.visit(news_url)
+    html = browser.html
+    soup = BeautifulSoup(html, "html.parser")
+    article = soup.find("div", class_='list_text')
+    news_title = article.find("div", class_="content_title").text
+    news_p = article.find("div", class_ ="article_teaser_body").text
+    output = [news_title, news_p]
+    return output
+
+# # JPL Mars Space Images - Featured Image
+def marsImage():
+    image_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+    browser.visit(image_url)
+    html = browser.html
+    soup = BeautifulSoup(html, "html.parser")
+    image = soup.find("img", class_="thumb")["src"]
+    featured_image_url = "https://www.jpl.nasa.gov" + image
+    return featured_image_url
+
+# # Mars Weather
+def marsWeather():
+    
+    import tweepy
+    # Twitter API Keys
+    def get_file_contents(filename):
+        try:
+            with open(filename, 'r') as f:
+                return f.read().strip()
+        except FileNotFoundError:
+            print("'%s' file not found" % filename)
+
+    consumer_key = get_file_contents('consumer_key')
+    consumer_secret = get_file_contents('consumer_secret')
+    access_token = get_file_contents('access_token')
+    access_token_secret = get_file_contents('access_token_secret')
+
+    # Setup Tweepy API Authentication
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+
+    target_user = "MarsWxReport"
+    tweet = api.user_timeline(target_user, count =1)
+    mars_weather = ((tweet)[0]['text'])
+    return mars_weather
+
+
+# # Mars Facts
+def marsFacts():
+    import pandas as pd
+    facts_url = "https://space-facts.com/mars/"
+    browser.visit(facts_url)
+    mars_data = pd.read_html(facts_url)
+    mars_data = pd.DataFrame(mars_data[0])
+    mars_data.columns = ["Description", "Value"]
+    mars_data = mars_data.set_index("Description")
+    mars_facts = mars_data.to_html(index = True, header =True)
+    return mars_facts
+
+
+# # Mars Hemispheres
+def marsHem():
+    import time 
+    hemispheres_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(hemispheres_url)
+    html = browser.html
+    soup = BeautifulSoup(html, "html.parser")
+    mars_hemisphere = []
+
+    products = soup.find("div", class_ = "result-list" )
+    hemispheres = products.find_all("div", class_="item")
+
+    for hemisphere in hemispheres:
+        title = hemisphere.find("h3").text
+        title = title.replace("Enhanced", "")
+        end_link = hemisphere.find("a")["href"]
+        image_link = "https://astrogeology.usgs.gov/" + end_link    
+        browser.visit(image_link)
+        html = browser.html
+        soup=BeautifulSoup(html, "html.parser")
+        downloads = soup.find("div", class_="downloads")
+        image_url = downloads.find("a")["href"]
+        dictionary = {"title": title, "img_url": image_url}
+        mars_hemisphere.append(dictionary)
+    return mars_hemisphere
