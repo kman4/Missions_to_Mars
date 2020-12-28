cde97404772484f94a4c3bcfba74db01625f6780
Kapil Pundhir <70111855+kman4@users.noreply.github.com>
2020-12-28 13:45:44 -0500

    commit

--- app.py
+++ app.py
@@ -1,22 +1,36 @@
 ### IMPORT DEPENDENCIES
 
 from flask import Flask, render_template, redirect
-from flask_pymongo import pymongo 
-import scape
+from flask_pymongo import PyMongo
+import scrape_mars
 
-# INITIATE YOUR CONFIGURATION 
+# Create an instance of Flask
 app = Flask(__name__)
-app.config["MONGO_URI"] = 'mongodb://localhost:27017/mars_db'
+
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
+# Route to render index.html template using data from Mongo
 @app.route("/")
 def index():
-    mars_data = db.collection.find_one()
-    return render_template('index.html', list=mars_data)
-                           
-@appl.route("/scrape")
+
+    # Find one record of data from the mongo database
+    mars_dict = mongo.db.mars_dict.find_one()
+    # Return template and data
+    return render_template("index.html", mars=mars_dict)
+
+
+@app.route("/scrape")
 def scrape():
-    scrape.scrape_all()
-    return redirect("/")                       
-                           
+  
+    mars_dict = mongo.db.mars_dict
+    mars_data = scrape_mars.scrape()
+    # Update the Mongo database using update and upsert=True
+    mars_dict.update({}, mars_data, upsert=True)
+    return redirect("/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True) 
  
